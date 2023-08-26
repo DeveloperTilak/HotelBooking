19d1bcd8d990d1e1d33c63844c55c3cc61dda97a
Developer Awdhesh <awdheshthinker@gmail.com>
2023-08-26 19:47:27 +0530

    working locally

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -1,16 +1,18 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+//imports
 import "./hotel.css";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
 import Mail from "../../components/mail/Mail";
 import Footer from "../../components/footer/Footer";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCircleArrowLeft,
   faCircleArrowRight,
   faCircleXmark,
   faLocationDot,
 } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
 
 const Hotel = () => {
   const [slideNumber, setSlideNumber] = useState(0);
